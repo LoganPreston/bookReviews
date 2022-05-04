d21f98dcad1a395201af6ad7e85ee26bc0ebc04d
logan preston <personal@loganpreston.com>
2022-05-03 21:38:35 -0500

    remove some comments, other formatting tweaks

--- bookReviews.go
+++ bookReviews.go
@@ -57,17 +57,13 @@ func readBooks() []string {
 }
 
 func getUrl(book, author string) string {
-	//get isbn from librarything?
-	//libraryThingUrlBase := "http://librarything.com/api/thingTitle/"
 
 	url := "https://www.googleapis.com/books/v1/volumes?q="
 	book = strings.Replace(book, " ", "%20", -1)
-	//book = "\"" + book + "\""
 	url += "intitle:" + book
 
 	if len(author) > 0 {
 		author = strings.Replace(author, " ", "%20", -1)
-		//author = "\"" + author + "\""
 		url += "+inauthor:" + author
 	}
 
@@ -87,12 +83,12 @@ func getUrlInfo(url string) ([]byte, error) {
 	if response, err = http.Get(url); err != nil {
 		return []byte{}, err
 	}
-	//fmt.Printf("%v\n", response)
+
 	//read the response
 	if responseData, err = ioutil.ReadAll(response.Body); err != nil {
 		return []byte{}, err
 	}
-	//fmt.Printf("%v\n",responseData)
+
 	return responseData, nil
 }
 
@@ -171,6 +167,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	defer file.Close()
 	writer := bufio.NewWriter(file)
 
 	line := "Title|Author|ISBN|Review|Review Count|Book Count\n"
@@ -185,15 +182,14 @@ func main() {
 		items := itemsInfo{}
 		bookInfo := strings.Split(book, "|")
 		url := getUrl(bookInfo[0], bookInfo[1])
-		//fmt.Printf("%s\n", url)
+
 		fmt.Printf("Checking into %s by %s\n", bookInfo[0], bookInfo[1])
 		responseBytes, _ := getUrlInfo(url)
 
-		//fmt.Printf("%v\n", responseBytes)
 		if err := json.Unmarshal(responseBytes, &items); err != nil {
 			fmt.Println(err.Error())
 		}
-		//fmt.Printf("%v\n", items)
+
 		avgRating, numReviews, bookCount := getBookRating(items.Items, bookInfo[0])
 
 		//get author, title. Default to input, else first with isbn
@@ -202,18 +198,14 @@ func main() {
 			title = items.Items[i].VolumeInfo.Title
 			author = items.Items[i].VolumeInfo.Authors
 			isbn = getIsbn(items.Items[i].VolumeInfo.Ids)
-			//fmt.Printf("%d, %d, %s\n", len(items.Items), i, isbn)
 		}
 
-		line := fmt.Sprintf("%s|%v|%s|%.2f|%d|%d\n", title, author, isbn,
-			avgRating, numReviews, bookCount)
+		line := fmt.Sprintf("%s|%v|%s|%.2f|%d|%d\n",
+			title, author, isbn, avgRating, numReviews, bookCount)
 		_, err := writer.WriteString(line)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 	}
 	writer.Flush()
-
-	//use isbn to get reviews from google books
-
 }
